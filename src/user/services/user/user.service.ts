7e37627bba8cb0c76fb07c84f3ecf51eb17b2c4b
emonsingha209 <emonsingha209@gmail.com>
2023-02-08 00:14:44 +0600

    user

--- src/user/services/user/user.service.ts
+++ src/user/services/user/user.service.ts
@@ -1,45 +1,8 @@
-import { Body, Injectable, Param, Query } from '@nestjs/common';
-
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
-   users=[
-    {
-        id:1,
-        email:'ratul@gmail.com',
-        name:'Ratul',
-    },
-    {
-        id:2,
-        email:'rahim@gmail.com',
-        name:'rahim',
-    },
-    {
-        id:3,
-        email:'sadman@gmail.com',
-        name:'sadman',
-    },
-   ];
-
-
-    findUserById(id:number){
-        return this.users.find((user)=>user.id===id);
-    }
-  
-      getAllUser(): string {
-        return "Get all user without id";
-      }
-    
-      getUser(@Param() data): string {
-        return "Get user with id " + data.id;
-      }
-      deleteUser(@Param() prm): string {
-        return "Delete user with id " + prm.id;
-      }
-      editUser(@Param() qar): string {
-        return "Edit user with id " + qar.id;
-      }
-      addUser(@Param() qar): string {
-        return "Create user with id "+qar.id;
-      }
+  getIndex():string {
+    return "User Index";
+  }
 }
