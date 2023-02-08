32e5e83981c7741cae8eaddc6a4b1e3fde73fd60
Ratul Bhattacharjee <77481058+Ratul41Bhatt@users.noreply.github.com>
2023-02-08 09:54:51 +0600

    admin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NutritionistModule } from './Nutritionist/nutritionist.module';
+import { AdminModule } from './admin/admin.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [NutritionistModule],
+  imports: [UserModule,NutritionistModule, AdminModule],
   controllers: [],
   providers: [],
 })
