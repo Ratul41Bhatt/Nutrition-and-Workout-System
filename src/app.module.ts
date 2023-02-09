b2e4bfcce0a5b688b4e6c4d336be07480338229c
emonsingha209 <emonsingha209@gmail.com>
2023-02-09 12:04:57 +0600

    Module updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { TrainerModule } from './Trainer/trainer.module';
+import { AdminModule } from "./admin/admin.module";
+import { NutritionistModule } from "./Nutritionist/nutritionist.module";
+import { UserModule } from "./user/user.module";
 @Module({
-  imports: [TrainerModule],
+  imports: [AdminModule, NutritionistModule, TrainerModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
