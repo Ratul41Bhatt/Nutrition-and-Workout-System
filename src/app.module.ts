30fa0a247df45a6825b718c80e2d707ee04d851e
SaadNasir-Drago <snasir2194@gmail.com>
2023-02-20 11:16:30 +0600

    trainer

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,37 +2,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { TrainerModule } from './Trainer/trainer.module';
-import { AdminModule } from "./admin/admin.module";
-import { NutritionistModule } from "./Nutritionist/nutritionist.module";
-import { UserModule } from "./user/user.module";
-
-
-@Module({
-  imports: [AdminModule, NutritionistModule, TrainerModule, UserModule, TypeOrmModule.forRoot(
-      { 
-       type: 'postgres',
-       host: 'localhost',
-       port: 5432,
-       username: 'postgres',
-       password: 'root',
-       database: 'postgres',
-       autoLoadEntities: true,
-       synchronize: true,
-     }
-  ),],
-  controllers: [AppController],
-
-  providers: [AppService],
-})
-
-export class AppModule {}
-
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 import { TrainerModule } from './Trainer/trainer.module';
 import { AdminModule } from "./admin/admin.module";
